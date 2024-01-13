6e376ebdcc10c818de6c138b2ec91e07ba460730
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2024-01-13 17:53:55 +0300

    rust: day-13

---
+++ rust/2023/day-13/benches/benchmarks.rs
@@ -0,0 +1,38 @@
+use day_13::*;
+
+fn main() {
+    // Run registered benchmarks.
+    divan::main();
+}
+
+#[divan::bench]
+fn part1() {
+    part1::process(divan::black_box(include_str!(
+        "../input1.txt",
+    )))
+    .unwrap();
+}
+
+#[divan::bench]
+fn part1_biscardi() {
+    part1_biscardi::process(divan::black_box(include_str!(
+        "../input1.txt",
+    )))
+    .unwrap();
+}
+
+#[divan::bench]
+fn part2() {
+    part2::process(divan::black_box(include_str!(
+        "../input2.txt",
+    )))
+    .unwrap();
+}
+
+#[divan::bench]
+fn part2_biscardi() {
+    part2_biscardi::process(divan::black_box(include_str!(
+        "../input2.txt",
+    )))
+    .unwrap();
+}
