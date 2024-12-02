3efb423a920e22839652bb5a039ffb6d42a9c47f
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2024-12-02 17:11:36 +0300

    aoc2024: day02

---
+++ rust/2024/README.md
@@ -0,0 +1,13 @@
+# AdventOfCode2024 :christmas_tree:
+Solutions to AoC 2024 in Rust :crab:
+
+## Usage
+```sh
+export AOC_SESSION=<session cookie>
+
+just create 01       # create new day, e.g. 01-25
+just test 01 1       # test day/part
+just run 01 1        # run day/part
+just bench 01 1      # benchmark day/part; use "" for both parts
+just dhat 01 1       # benchmark day/part; use "" for both parts
+```
---
+++ rust/2024/day02/src/part1.rs
@@ -0,0 +1,58 @@
+use crate::custom_error::AocError;
+
+fn is_safe(report: &Vec<i32>) -> bool {
+    let mut direction = 0; // 0 - undefined, 1 - increasing, -1 - decreasing
+
+    for pair in report.windows(2) {
+        let diff = pair[1] - pair[0];
+
+        // Check diff condition
+        if diff.abs() < 1 || diff.abs() > 3 {
+            return false;
+        }
+
+        let current_direction = if diff > 0 { 1 } else { -1 };
+
+        if direction == 0 {
+            direction = current_direction;
+        }
+
+        // Check direction condition
+        if direction != current_direction {
+            return false;
+        }
+    }
+    return true;
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let safe = input
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .filter(is_safe)
+        .count();
+
+    Ok(safe.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("2", process(input)?);
+        Ok(())
+    }
+}
